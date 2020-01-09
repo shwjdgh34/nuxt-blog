c9ad47d7e07facb9586f1dd8e1384191368d6cc0
shwjdgh34 <nossi8128@gmail.com>
2020-01-09 22:46:06 +0900

    Create PostPreview component

---
+++ components/Posts/PostPreview.vue
@@ -0,0 +1,76 @@
+<template>
+  <nuxt-link :to="`/posts/${id}`" class="post-preview">
+    <article>
+      <!--save images to assets-->
+      <!-- style="background-image: url('https://image.fmkorea.com/files/attach/new/20190916/2063168106/1524368855/2190443186/f87ba14f24b231dec02134f7f9577cbb.jpg')" -->
+      <div class="post-thumbnail" :style="{backgroundImage: `url(${thumbnail})`}"></div>
+      <div class="post-content">
+        <h1>{{title}}</h1>
+        <p>{{descript}}</p>
+      </div>
+    </article>
+  </nuxt-link>
+</template>
+
+<script>
+export default {
+  name: 'PostPreview',
+  props: {
+    id: {
+      type: String,
+      required: true
+    },
+    title: {
+      type: String,
+      required: true
+    },
+    descript: {
+      type: String,
+      required: true
+    },
+    thumbnail: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+.post-preview {
+  border: 1px solid #ccc;
+  box-shadow: 0 2px 2px #ccc;
+  background-color: white;
+  width: 90vw;
+}
+
+a {
+  text-decoration: none;
+  color: black;
+}
+
+.post-thumbnail {
+  width: 100%;
+  height: 180vw;
+  background-position: center;
+  background-size: cover;
+}
+@media (min-width: 850px) {
+  .post-preview {
+    width: 400px;
+    margin: 10px;
+  }
+  .post-thumbnail {
+    height: 800px;
+  }
+}
+.post-content {
+  padding: 10px;
+  text-align: center;
+}
+
+a:hover .post-content,
+a:active .post-content {
+  background-color: #ccc;
+}
+</style>
