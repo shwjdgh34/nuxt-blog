0feb0c9c9476b5643092c3cd361b8d1f0839c5f6
shwjdgh34 <nossi8128@gmail.com>
2020-01-19 20:34:37 +0900

    Add nuxtServerInit to set loadedPosts on the server side before init

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -4,43 +4,14 @@
   </div>
 </template>
 <script>
-import { mapGetters } from 'vuex'
+import { mapState, mapGetters } from 'vuex'
 import PostList from '@/components/Posts/PostList.vue'
 export default {
   components: {
     PostList
   },
-  asyncData(context) {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        resolve({
-          loadedPosts: [
-            {
-              id: '1',
-              title: 'post 1',
-              previewText: 'this is the first post',
-              thumbnail:
-                'https://image.fmkorea.com/files/attach/new/20190916/2063168106/1524368855/2190443186/f87ba14f24b231dec02134f7f9577cbb.jpg'
-            },
-            {
-              id: '2',
-              title: 'post 2',
-              previewText: 'this is the second post',
-              thumbnail:
-                'https://image.fmkorea.com/files/attach/new/20190916/2063168106/1524368855/2190443186/b1426e1a5cc005b19ba22962482931b0.jpg'
-            }
-          ]
-        })
-        reject(new Error())
-      }, 1500)
-    })
-      .then(data => {
-        return data
-      })
-      .catch(e => context.error(e))
-  },
-  created() {
-    this.$store.dispatch('post/setPosts', this.loadedPosts)
+  computed: {
+    ...mapGetters('post', ['loadedPosts'])
   }
 }
 </script>
---
+++ store/index.js
@@ -0,0 +1,21 @@
+export const actions = {
+  nuxtServerInit({ commit }) {
+    console.log('nonobabo')
+    commit('post/setPosts', [
+      {
+        id: '1',
+        title: 'post 1',
+        previewText: 'this is the first post',
+        thumbnail:
+          'https://image.fmkorea.com/files/attach/new/20190916/2063168106/1524368855/2190443186/f87ba14f24b231dec02134f7f9577cbb.jpg'
+      },
+      {
+        id: '2',
+        title: 'post 2',
+        previewText: 'this is the second post',
+        thumbnail:
+          'https://image.fmkorea.com/files/attach/new/20190916/2063168106/1524368855/2190443186/b1426e1a5cc005b19ba22962482931b0.jpg'
+      }
+    ])
+  }
+}
