a3dc078c4ee71ccb2bc464198751abf11fed36f4
shwjdgh34 <nossi8128@gmail.com>
2020-01-20 09:24:35 +0900

    Edit post

--- pages/admin/_postId/index.vue
+++ pages/admin/_postId/index.vue
@@ -1,16 +1,37 @@
 <template>
   <div class="admin-post-page">
     <section class="update-form">
-      <AdminPostForm :post="loadedPost" />
+      <AdminPostForm :post="loadedPost" @submit="onSubmitted" />
     </section>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 import AdminPostForm from '@/components/Admin/AdminPostForm'
 export default {
   components: {
     AdminPostForm
+  },
+  asyncData(context) {
+    return axios
+      .get(
+        `https://nuxt-blog-50ca9.firebaseio.com/posts/${context.route.params.postId}.json`
+      )
+      .then(({ data }) => {
+        return { loadedPost: data }
+      })
+  },
+  methods: {
+    onSubmitted(editedPost) {
+      axios
+        .put(
+          `https://nuxt-blog-50ca9.firebaseio.com/posts/${this.$route.params.postId}.json`,
+          { ...editedPost, updatedAt: new Date() }
+        )
+        .then(console.log)
+        .catch(console.log)
+    }
   }
 }
 </script>
