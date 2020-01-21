f779f900376bf47eece6422821cada79feb95029
shwjdgh34 <nossi8128@gmail.com>
2020-01-21 22:09:47 +0900

    Apply date filter using by plugin

--- nuxt.config.js
+++ nuxt.config.js
@@ -27,7 +27,7 @@ export default {
   /*
    ** Plugins to load before mounting the App
    */
-  plugins: ['~plugins/core-components.js'],
+  plugins: ['~plugins/core-components.js', '~plugins/date-filter.js'],
   /*
    ** Nuxt.js dev-modules
    */
--- pages/posts/_id/index.vue
+++ pages/posts/_id/index.vue
@@ -3,7 +3,7 @@
     <section class="posts">
       <h1 class="post-title">{{ loadedPost.title }}</h1>
       <div class="post-details">
-        <div class="post-detail">{{ loadedPost.updatedAt }}</div>
+        <div class="post-detail">{{ loadedPost.updatedAt | date }}</div>
         <div class="post-detail">{{ loadedPost.author }}</div>
       </div>
       <p class="post-content">{{ loadedPost.content }}</p>
